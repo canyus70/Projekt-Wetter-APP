e07ee0340361401cb20f3335f98802f12d667dcf
canyus70 <canyus@gmx.de>
2023-11-20 16:22:16 +0100

    die erste version (1) steht

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,4 @@
 
-// fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=034b30eb230e77ea19f67cc74c9589f1')
-//             .then(response=> {
-//                 if(response.ok === false) {
-//                     throw new Error ("Fetch konnte nicht geladen werden")
-//                 }
-//                 return response.json();
-//                 // console.log(json())
-//             })
-//             .then (data => {
-            
-//                     const temperatur = data.main.temp;
-//                     const gefühlteTemperatur = data.main.feels_like;
-                    
-//                     console.log(temperatur);
-                    
-//                 })
-
-
-
 const version1 = () => {
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=034b30eb230e77ea19f67cc74c9589f1')
             .then(response=> {
@@ -35,8 +16,25 @@ const version1 = () => {
                     const wetterBeschreibung = data.weather[0].description;
                     const lat = data.coord.lat;
                     const lon = data.coord.lon;
-                    
-                    console.log(lon);
+                    const location = "London";
+                    const minTemp = data.main.temp_min;
+                    const maxTemp = data.main.temp_max;
+                    const luftDruck= data.main.pressure;
+                    const windGeschwindigKeit = data.wind.speed;
+                    const windRichtung = data.wind.deg;
+                    const boehen = data.wind.gust;
+
+                    document.getElementById("wetterlage").textContent= wetter;
+                    document.getElementById("temperatur").textContent= (temperatur -273.15).toFixed(0) +" "+ "°C";
+                    document.getElementById("location").textContent= location;
+                    document.getElementById("maxTemp").textContent= (maxTemp -273.15).toFixed(0) +" "+ "°C";
+                    document.getElementById("minTemp").textContent= (minTemp -273.15).toFixed(0) +" "+ "°C";
+                    document.getElementById("gefühltTemp").textContent= (gefühlteTemperatur -273.15).toFixed(0) +" "+ "°C";
+                    document.getElementById("luftdruck").textContent= luftDruck+ " hp";
+                    document.getElementById("windgeschwindigkeit").textContent= windGeschwindigKeit+ " km/h";
+                    document.getElementById("windrichtung").textContent= windRichtung + " °";
+                    document.getElementById("böhen").textContent= boehen + " km/h";
+                    console.log(windRichtung);
                     
                 })
 }
